07d0810c7c935d98581d946d6396c3ea6838d59a
lovetownyuan <meifanglalala@gmail.com>
2024-02-21 16:37:38 +0800

    Updates

---
+++ assets/HomeView-DbJtjOoR.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as t,a as c}from"./index-CRT6yWqx.js";const o={},_={class:""},a=c("p",{class:"text-center"},"首頁",-1),n=[a];function r(d,i){return s(),t("div",_,n)}const l=e(o,[["render",r]]);export{l as default};
---
+++ assets/OrderView-Ci3W_J8m.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-CRT6yWqx.js";const r={},o={class:"text-center"};function s(n,_){return c(),t("div",o,"後台訂單列表")}const d=e(r,[["render",s]]);export{d as default};
